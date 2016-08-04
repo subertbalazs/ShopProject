2a5aa600880fc918c213778958d4cc3194fbf8cf
subertbalazs <balazs.subert@gmail.com>
2016-08-04 13:37:08 +0200

    Refactor tests

--- src/MainTest.java
+++ src/MainTest.java
@@ -9,6 +9,33 @@ import org.junit.Test;
 
 public class MainTest {
 
+
+	@Test
+	public void checkTodaysDateFor_getDateTimeNow() {
+		Main junit = new Main();
+		String result = junit.getDateTimeNow();
+		String now = LocalDateTime.now().toString();
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date nowDate;
+		try {
+			nowDate = sdf.parse(now);
+			Date getFromNowFunction = sdf.parse(result);
+			assertEquals(nowDate, getFromNowFunction);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void lastCharTestFor_getDateTimeNow() {
+		Main junit = new Main();
+		int result = junit.getDateTimeNow().substring(9).lastIndexOf(9);
+		int date = LocalDateTime.now().toString().substring(9).lastIndexOf(9);
+		assertEquals(result, date);
+	}
+
 	@Test
 	public void checkReturnTypeFor_checkWarranty_withLess_returnTrue() {
 		Main junit = new Main();
@@ -40,31 +67,20 @@ public class MainTest {
 			e.printStackTrace();
 		}
 	}
-
 	@Test
-	public void checkTodaysDateFor_getDateTimeNow() {
+	public void checkReturnTypeFor_checkWarranty_withMore_returnFalse() {
 		Main junit = new Main();
-		String result = junit.getDateTimeNow();
-		String now = LocalDateTime.now().toString();
-
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		Date nowDate;
+		Date warranty;
 		try {
-			nowDate = sdf.parse(now);
-			Date getFromNowFunction = sdf.parse(result);
-			assertEquals(nowDate, getFromNowFunction);
+			warranty = sdf.parse("2012-11-11");
+			Date now = sdf.parse("2012-11-13");
+			boolean result = junit.checkWarranty(warranty, now);
+			assertEquals(result, false);
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
-	@Test
-	public void lastCharTestFor_getDateTimeNow() {
-		Main junit = new Main();
-		int result = junit.getDateTimeNow().substring(9).lastIndexOf(9);
-		int date = LocalDateTime.now().toString().substring(9).lastIndexOf(9);
-		assertEquals(result, date);
-	}
-
 }
+
