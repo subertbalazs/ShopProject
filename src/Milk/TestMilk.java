df23db59ed391ba625303707afd8f489abf41873
subertbalazs <balazs.subert@gmail.com>
2016-08-16 11:32:11 +0200

    Some more Unittests for the getter methods

--- src/Milk/TestMilk.java
+++ src/Milk/TestMilk.java
@@ -1,11 +1,109 @@
 package Milk;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Hashtable;
 
 import org.junit.Test;
 
 public class TestMilk {
+
+	@Test
+	public void test_getCapacity_ReturnTrue_inCaseOfCorrectValue() {
+		Milk milk = new Milk(1111, 1000, "1999-11-11", 1, 230, "test");
+		int capacity = 1000;
+		int result = milk.getCapacity();
+		assertEquals(result, capacity);
+	}
+
+	@Test
+	public void test_getCapacity_ReturnFalse_inCaseOfIncorrectValue() {
+		Milk milk = new Milk(1111, 1000, "1999-11-11", 1, 230, "test");
+		Integer capacity = 500;
+		Integer result = milk.getCapacity();
+		assertFalse(capacity.equals(result));
+	}
+
+	@Test
+	public void test_getWarrant_ReturnTrue_inCaseOfCorrectValue() {
+		Milk milk = new Milk(1111, 1000, "1999-11-11", 1, 230, "test");
+		String warranty = "1999-11-11";
+		String result = milk.getWarrant();
+		assertEquals(result, warranty);
+	}
+
+	@Test
+	public void test_getWarrant_ReturnTrue_inCaseOfIncorrectValue() {
+		Milk milk = new Milk(1111, 1000, "1999-11-11", 1, 230, "test");
+		String warranty = "2111-11-11";
+		String result = milk.getWarrant();
+		assertFalse(warranty.equals(result));
+	}
+
+	@Test
+	public void test_getDripping_ReturnTrue_inCaseOfCorrectValue() {
+		Milk milk = new Milk(1111, 1000, "1999-11-11", 1.0, 230, "test");
+		Double dripping = 1.0;
+		Double result = milk.getDripping();
+		assertTrue(dripping.equals(result));
+	}
+
+	@Test
+	public void test_getDripping_ReturnFalse_inCaseOfIncorrectValue() {
+		Milk milk = new Milk(1111, 1000, "1999-11-11", 1.0, 230, "test");
+		Double dripping = 2.0;
+		Double result = milk.getDripping();
+		assertFalse(dripping.equals(result));
+	}
+
+	@Test
+	public void test_getPrice_ReturnTrue_inCaseOfCorrectValue() {
+		Milk milk = new Milk(1111, 1000, "1999-11-11", 1, 230, "test");
+		long price = 230;
+		long result = milk.getPrice();
+		assertEquals(result, price);
+	}
+
+	@Test
+	public void test_getPrice_ReturnFalse_inCaseOfIncorrectValue() {
+		Milk milk = new Milk(1111, 1000, "1999-11-11", 1, 230, "test");
+		Long price = (long) 0;
+		Long result = milk.getPrice();
+		assertFalse(price.equals(result));
+	}
+
+	@Test
+	public void test_getCompany_ReturnTrue_inCaseOfCorrectValue() {
+		Milk milk = new Milk(1111, 1000, "1999-11-11", 1, 230, "test");
+		String company = "test";
+		String result = milk.getCompany();
+		assertEquals(result, company);
+	}
+
+	@Test
+	public void test_getCompany_ReturnFalse_inCaseOfIncorrectValue() {
+		Milk milk = new Milk(1111, 1000, "1999-11-11", 1, 230, "test");
+		String company = "hello";
+		String result = milk.getCompany();
+		assertFalse(company.equals(result));
+	}
+
+	@Test
+	public void test_getBarCode_ReturnTrue_inCaseOfCorrectValue() {
+		Milk milk = new Milk(1111, 1000, "1999-11-11", 1, 230, "test");
+		Long barCode = (long) 1111;
+		Long result = milk.getBarCode();
+		assertEquals(result, barCode);
+	}
+
+	@Test
+	public void test_getBarCode_ReturnFalse_inCaseOfIncorrectValue() {
+		Milk milk = new Milk(1111, 1000, "1999-11-11", 1, 230, "test");
+		Long price = (long) 1000;
+		Long result = milk.getBarCode();
+		assertFalse(price.equals(result));
+	}
 
 	@Test
 	public void test_getWarrantyByBarCode_ReturnBarCode_inCaseOfEmptyHashTable() {
--- src/Store/TestStore.java
+++ src/Store/TestStore.java
@@ -1,5 +1,6 @@
 package Store;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import java.util.Hashtable;
 
@@ -8,6 +9,46 @@ import org.junit.Test;
 import Milk.Milk;
 
 public class TestStore {
+
+	@Test
+	public void test_getName_ReturnTrue_inCaseOfCorrectValue() {
+		Store store = new Store("TestBolt", "Miskolc", "The Boss");
+		String name = "TestBolt";
+		String result = store.getName();
+		assertEquals(result, name);
+	}
+
+	@Test
+	public void test_getName_ReturnFalse_inCaseOfIncorrectValue() {
+		Store store = new Store("TestBolt", "Miskolc", "The Boss");
+		String name = "Boltocska";
+		String result = store.getName();
+		assertFalse(name.equals(result));
+	}
+
+	@Test
+	public void test_getAddress_ReturnTrue_inCaseOfCorrectValue() {
+		Store store = new Store("TestBolt", "Miskolc", "The Boss");
+		String address = "Miskolc";
+		String result = store.getAddress();
+		assertEquals(result, address);
+	}
+
+	@Test
+	public void test_getOwner_ReturnFalse_inCaseOfIncorrectValue() {
+		Store store = new Store("TestBolt", "Miskolc", "The Boss");
+		String owner = "Nem Boss";
+		String result = store.getOwner();
+		assertFalse(owner.equals(result));
+	}
+
+	@Test
+	public void test_getOwner_ReturnTrue_inCaseOfCorrectValue() {
+		Store store = new Store("TestBolt", "Miskolc", "The Boss");
+		String owner = "The Boss";
+		String result = store.getOwner();
+		assertEquals(result, owner);
+	}
 
 	@Test
 	public void test_isTHereAnyMilk_returnTrue_inCaseOfEmptyHashTable() {
