1af0711638210241d261a7186ebd92e9d3d68487
subertbalazs <balazs.subert@gmail.com>
2016-08-23 18:00:18 +0200

    Delete SemiLongLifeMilk.java

--- src/Milk/SemiLongLifeMilk.java
+++
@@ -1,9 +0,0 @@
-package Milk;
-
-public class SemiLongLifeMilk extends Milk {
-
-	public SemiLongLifeMilk(long barCode, int capacity, String warrant, double dripping, String company) {
-		super(barCode, capacity, warrant, dripping, company);
-	}
-
-}
