cf22da2e694b9d4e173db4aa6af28f53488cdad2
subertbalazs <balazs.subert@gmail.com>
2016-08-23 13:04:47 +0200

    Refactor Milk class

--- src/Products/Milk.java
+++ src/Products/Milk.java
@@ -2,12 +2,6 @@ package products;
 
 public abstract class Milk extends Food {
 
-	public static final int LITER = 1000;
-	public static final int HALF_LITER = 500;;
-	public static final int GLASS = 250;
-	public static final double WHOLE_FAT = 1.5;
-	public static final double REDUCED_FAT = 2.8;
-
 	private int capacity;
 	private double dripping;
 
