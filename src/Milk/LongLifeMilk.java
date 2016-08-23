e550ddca1be9844fedccbeff49a6fb32c48b11fc
subertbalazs <balazs.subert@gmail.com>
2016-08-23 18:00:04 +0200

    Delete LongLifeMilk.java

--- src/Milk/LongLifeMilk.java
+++
@@ -1,8 +0,0 @@
-package Milk;
-
-public class LongLifeMilk extends Milk {
-
-	public LongLifeMilk(long barCode, int capacity, String warrant, double dripping, String company) {
-		super(barCode, capacity, warrant, dripping, company);
-	}
-}
